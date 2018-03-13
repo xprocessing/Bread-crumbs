f03465d1ccdf395405bc47fe9c4309c5796d262e
Neo <xprocessing@live.com>
2018-03-13 19:12:47 +0800

    拉取TOP100
    拉取TOP100商品搜索流量，待表格输出

--- TOP100.js
+++ TOP100.js
@@ -1,11 +1,31 @@
-//获取当前页面的商品ID，并根据商品ID进行拉取淘宝数据。
-var GetID;
-GetID = document.querySelectorAll('.info-wrapper a');
-for (each in GetID) {
+//获取TOP100的搜索流量
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://sycm.taobao.com/mq/rank/listItems.json?cateId=50006846&categoryId=50006846&dateRange=2018-03-06|2018-03-12&dateRangePre=2018-03-06|2018-03-12&dateType=recent7&dateTypePre=recent7&device=0&devicePre=0&itemDetailType=1&keyword=&orderDirection=desc&orderField=payOrdCnt&page=1&pageSize=30&rankTabIndex=0&rankType=1&seller=-1&view=rank", true);
+xhr.send();
+
+//上面先执行获得宝贝ID脏数据
+
+//获得宝贝ID数组
+var TOP100DATAJSON = JSON.parse(xhr.responseText.toString());
+var TOP100DATAArray=[];
+TOP100DATAArray=TOP100DATAJSON.content.data.data;
+//console.log(TOP100DATAArray[1]);
+var GetID =[];
+for (var i=0;i<TOP100DATAArray.length;i++)
+{
+    GetID[i]=TOP100DATAArray[i].itemId;
+   // console.log(GetID[i]);
+}
+
+console.table(GetID);
+
+//根据ID，获取搜索流量
+
+var SUVs=[]
+for (var N=0;N<GetID.length;N++) {
   var theID = "";
-  if (typeof GetID[each].href !== "undefined") {
-    theID = GetID[each].href.replace(/[^0-9]+/g, '');
-    //theID = GetID[each].href.match(/\d+/g);
+  theID = GetID[N];
+ 
     //console.log(theID);
     ////循环输出手淘搜索
 
@@ -23,6 +43,9 @@ for (each in GetID) {
             if (SearchNumTextdata[eachPageName].pageName == "手淘搜索") {
         
                 console.log("宝贝"+theID+SearchNumTextdata[eachPageName].pageName +"UV="+ SearchNumTextdata[eachPageName].uv);
+                //把搜索数据存到数组
+                SUVs[N]=SearchNumTextdata[eachPageName].uv;
+
             }
         
         }
@@ -39,13 +62,8 @@ for (each in GetID) {
     /////循环输出手淘搜索
 
 
-  }
+
 
 
 
 }
-
-
-
-
-
