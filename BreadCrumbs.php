2f0b1d3c998f0b713672c06adb487c4213ff90b5
Neo <xprocessing@live.com>
2018-09-15 17:51:54 +0800

    php常用代码块
    php常用代码块

---
+++ BreadCrumbs.php
@@ -0,0 +1,53 @@
+<?php 
+//php 常用代码块：https://blog.csdn.net/username222/article/details/78949614
+
+class Site { 
+  /* 成员变量 */ 
+  var $url; 
+  var $title; 
+   
+  /* 成员函数 */ 
+  function setUrl($par){ 
+     $this->url = $par; 
+  } 
+   
+  function getUrl(){ 
+     echo $this->url . PHP_EOL; 
+  } 
+   
+  function setTitle($par){ 
+     $this->title = $par; 
+  } 
+   
+  function getTitle(){ 
+     echo $this->title . PHP_EOL; 
+  } 
+} 
+
+$runoob = new Site; 
+$taobao = new Site; 
+$google = new Site; 
+
+// 调用成员函数，设置标题和URL 
+$runoob->setTitle( "哈哈哈哈哈哈哈哈哈哈哈哈哈" ); 
+$taobao->setTitle( "淘宝" ); 
+$google->setTitle( "Google 搜索" ); 
+
+$runoob->setUrl( 'www.runoob.com' ); 
+$taobao->setUrl( 'www.taobao.com' ); 
+$google->setUrl( 'www.google.com' ); 
+
+// 调用成员函数，获取标题和URL 
+$runoob->getTitle(); 
+$taobao->getTitle(); 
+$google->getTitle(); 
+
+$runoob->getUrl(); 
+$taobao->getUrl(); 
+$google->getUrl(); 
+$google->setTitle("000000000000000000000000000000");
+
+
+
+
+?>
